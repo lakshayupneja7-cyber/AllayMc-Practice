995b2c40761e20674966372a8759dfea23c02673
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-19 23:01:51 +0530

    Update PlayerManager.java

--- src/main/java/com/allaymc/practice/player/PlayerManager.java
+++ src/main/java/com/allaymc/practice/player/PlayerManager.java
@@ -1,1 +1,18 @@
+package com.allaymc.practice.player;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class PlayerManager {
+
+    private final Map<UUID, PracticePlayer> players = new HashMap<>();
+
+    public PracticePlayer getOrCreate(UUID uuid) {
+        return players.computeIfAbsent(uuid, PracticePlayer::new);
+    }
+
+    public void remove(UUID uuid) {
+        players.remove(uuid);
+    }
+}
