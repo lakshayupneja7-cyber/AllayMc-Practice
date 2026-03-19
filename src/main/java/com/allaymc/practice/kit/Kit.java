4ac162d94dad9c32f3a749d1a2e15150f1dfaacf
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-19 23:02:51 +0530

    Update Kit.java

--- src/main/java/com/allaymc/practice/kit/Kit.java
+++ src/main/java/com/allaymc/practice/kit/Kit.java
@@ -1,1 +1,34 @@
+package com.allaymc.practice.kit;
 
+import org.bukkit.inventory.ItemStack;
+
+public class Kit {
+
+    private final String name;
+    private final ItemStack[] inventoryContents;
+    private final ItemStack[] armorContents;
+    private final ItemStack offHand;
+
+    public Kit(String name, ItemStack[] inventoryContents, ItemStack[] armorContents, ItemStack offHand) {
+        this.name = name;
+        this.inventoryContents = inventoryContents;
+        this.armorContents = armorContents;
+        this.offHand = offHand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ItemStack[] getInventoryContents() {
+        return inventoryContents;
+    }
+
+    public ItemStack[] getArmorContents() {
+        return armorContents;
+    }
+
+    public ItemStack getOffHand() {
+        return offHand;
+    }
+}
