378039f97e70a08853b47250d9b6f566f5d33211
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-19 23:09:48 +0530

    Update MatchListener.java

--- src/main/java/com/allaymc/practice/listener/MatchListener.java
+++ src/main/java/com/allaymc/practice/listener/MatchListener.java
@@ -8,7 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.food.FoodLevelChangeEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 public class MatchListener implements Listener {
 
