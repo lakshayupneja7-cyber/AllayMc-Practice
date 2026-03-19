c95cc5858a57fbaad853805acb582a3f17096a3c
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-19 23:05:27 +0530

    Update PlayerConnectionListener.java

--- src/main/java/com/allaymc/practice/listener/PlayerConnectionListener.java
+++ src/main/java/com/allaymc/practice/listener/PlayerConnectionListener.java
@@ -1,1 +1,30 @@
+package com.allaymc.practice.listener;
 
+import com.allaymc.practice.AllayMcPractice;
+import com.allaymc.practice.player.PlayerState;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class PlayerConnectionListener implements Listener {
+
+    private final AllayMcPractice plugin;
+
+    public PlayerConnectionListener(AllayMcPractice plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        var profile = plugin.getPlayerManager().getOrCreate(event.getPlayer().getUniqueId());
+        profile.setState(PlayerState.LOBBY);
+        plugin.getMatchManager().resetToSpawn(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        plugin.getMatchManager().handleQuit(event.getPlayer());
+        plugin.getPlayerManager().remove(event.getPlayer().getUniqueId());
+    }
+}
