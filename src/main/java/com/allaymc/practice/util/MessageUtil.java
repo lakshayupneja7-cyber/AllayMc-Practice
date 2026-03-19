1ad5eb4343b64a8d5e194c6e36d99ab298c7966d
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-19 23:05:52 +0530

    Update MessageUtil.java

--- src/main/java/com/allaymc/practice/util/MessageUtil.java
+++ src/main/java/com/allaymc/practice/util/MessageUtil.java
@@ -1,1 +1,11 @@
+package com.allaymc.practice.util;
 
+public class MessageUtil {
+
+    private MessageUtil() {
+    }
+
+    public static String color(String text) {
+        return text == null ? "" : text.replace("&", "§");
+    }
+}
