57ea0fd17c725f7d6ee1046cfa8ba5ed22c49616
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-19 23:02:23 +0530

    Update Arena.java

--- src/main/java/com/allaymc/practice/arena/Arena.java
+++ src/main/java/com/allaymc/practice/arena/Arena.java
@@ -1,1 +1,44 @@
+package com.allaymc.practice.arena;
 
+import org.bukkit.Location;
+
+public class Arena {
+
+    private final String name;
+    private final String kitName;
+    private final Location spawn1;
+    private final Location spawn2;
+    private ArenaState state;
+
+    public Arena(String name, String kitName, Location spawn1, Location spawn2) {
+        this.name = name;
+        this.kitName = kitName;
+        this.spawn1 = spawn1;
+        this.spawn2 = spawn2;
+        this.state = ArenaState.FREE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getKitName() {
+        return kitName;
+    }
+
+    public Location getSpawn1() {
+        return spawn1;
+    }
+
+    public Location getSpawn2() {
+        return spawn2;
+    }
+
+    public ArenaState getState() {
+        return state;
+    }
+
+    public void setState(ArenaState state) {
+        this.state = state;
+    }
+}
