0a6d1fd5d4253ac6e3ce1362928ac696a5b8cda6
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-03-19 23:01:36 +0530

    Update PracticePlayer.java

--- src/main/java/com/allaymc/practice/player/PracticePlayer.java
+++ src/main/java/com/allaymc/practice/player/PracticePlayer.java
@@ -1,1 +1,26 @@
+package com.allaymc.practice.player;
 
+import java.util.UUID;
+
+public class PracticePlayer {
+
+    private final UUID uuid;
+    private PlayerState state;
+
+    public PracticePlayer(UUID uuid) {
+        this.uuid = uuid;
+        this.state = PlayerState.LOBBY;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public PlayerState getState() {
+        return state;
+    }
+
+    public void setState(PlayerState state) {
+        this.state = state;
+    }
+}
